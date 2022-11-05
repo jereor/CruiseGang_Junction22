504f003282f2d3cfe6e7529bcd0a05e521b1fb21
Jere Orava <jere.orava@hotmail.com>
2022-11-05 12:17:01 +0200

    Updated AppMonobehaviour

--- Assets/Scripts/AppMonobehaviour.cs
+++ Assets/Scripts/AppMonobehaviour.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AppMonobehaviour : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
